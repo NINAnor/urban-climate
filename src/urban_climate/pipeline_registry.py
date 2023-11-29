88f7878ccb7bc3554309743a0f97df9f36594608
Willeke A'Campo (linux vdi) <willeke.acampo@nina.no>
2023-11-29 21:25:01 +0100

    add: single pipeline functionality

--- src/urban_climate/pipeline_registry.py
+++ src/urban_climate/pipeline_registry.py
@@ -1,16 +1,26 @@
 """Project pipelines."""
 from __future__ import annotations
+from typing import Dict
 
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
+# import pipelines here
+from .pipelines import raster_processing, data_science
 
 
-def register_pipelines() -> dict[str, Pipeline]:
+
+def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
 
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
+    #pipelines = find_pipelines()
+    
+    pipelines = {
+        "raster_processing": raster_processing.create_pipeline(),
+        "data_science": data_science.create_pipeline(),
+    }
+    
     pipelines["__default__"] = sum(pipelines.values())
     return pipelines
